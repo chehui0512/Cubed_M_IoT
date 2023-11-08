03aa54a3508d5ee20ba6aa258045d73f787bdae1
sodiffi2 <11136005@ntub.edu.tw>
2023-11-09 00:25:14 +0800

    移除 - 多餘程式

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:e_fu/home.dart';
-import 'package:e_fu/pages/event/event_now_result.dart';
-
-import 'package:e_fu/pages/exercise/detail.dart';
-import 'package:e_fu/pages/exercise/history.dart';
-import 'package:e_fu/pages/exercise/insert.dart';
-import 'package:e_fu/pages/exercise/invite.dart';
-import 'package:e_fu/pages/mo/hide_mo_list.dart';
-import 'package:e_fu/pages/mo/mo_detail.dart';
-import 'package:e_fu/pages/mo/mo_list.dart';
-import 'package:e_fu/pages/plan/plan.dart';
-import 'package:e_fu/pages/plan/plan_insert.dart';
-import 'package:e_fu/pages/profile/profile_edit.dart';
-import 'package:e_fu/pages/profile/profile_goal.dart';
-import 'package:e_fu/pages/profile/profile_update.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-import 'package:e_fu/pages/event/event.dart';
 import 'package:e_fu/sign.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'my_data.dart';
@@ -31,8 +15,6 @@ void main() {
     ..loadingStyle = EasyLoadingStyle.light
     ..indicatorSize = 90.0
     ..maskType = EasyLoadingMaskType.custom
-    // ..displayDuration = const Duration(seconds: 2)
-    // ..indicatorType = EasyLoadingIndicatorType.fadingCircle
     ..maskColor = Colors.grey.withOpacity(0.5)
     ..userInteractions = true
     ..backgroundColor = MyTheme.backgroudColor
@@ -91,24 +73,6 @@ class MyappState extends State<MyApp> {
       routes: <String, WidgetBuilder>{
         Home.routeName: (_) => Home(userName: userName),
         Login.routeName: (_) => const Login(),
-        Event.routeName: (_) => Event(userName: userName),
-        ProfileUpdate.routeName: (_) => const ProfileUpdate(),
-        EventNowResult.routeName: (_) => EventNowResult(userName: userName),
-        MoList.routeName: (_) => MoList(userName: userName),
-        HindMoList.routeName: (_) => HindMoList(userName: userName),
-        InsertInvite.routeName: (_) => InsertInvite(userName: userName),
-        Profile.routeName: (_) => Profile(userName: userName),
-        // ProfileGoal.routeName: (_) => ProfileGoal(userName: userName),
-        InvitePage.routeName: (_) => InvitePage(userName: userName),
-        HistoryDetailPerson.routeName: (_) => HistoryDetailPerson(
-              userName: userName,
-            ),
-        HistoryDetailPage.routeName: (_) => HistoryDetailPage(
-              userName: userName,
-            ),
-        PlanPage.routeName: (_) => PlanPage(userName: userName),
-        PlanInsertPage.routeName: (_) => PlanInsertPage(userName: userName),
-        // MoDetail.routeName: (_) => MoDetail(userName: userName),
       },
     );
   }
--- lib/pages/profile/profile.dart
+++ lib/pages/profile/profile.dart
@@ -2,9 +2,9 @@ import 'package:e_fu/main.dart';
 import 'package:e_fu/module/box_ui.dart';
 import 'package:e_fu/module/page.dart';
 import 'package:e_fu/my_data.dart';
-import 'package:e_fu/pages/plan/plan.dart';
+
 import 'package:e_fu/pages/profile/analysis.dart';
-import 'package:e_fu/pages/profile/profile_edit.dart';
+
 import 'package:e_fu/request/user/account.dart';
 import 'package:e_fu/request/user/get_user_data.dart';
 import 'package:flutter/material.dart';
@@ -21,18 +21,6 @@ class ProfileInfo extends StatefulWidget {
   ProfileCreateState createState() => ProfileCreateState();
 }
 
-// class RawDataSet {
-//   RawDataSet({
-//     required this.title,
-//     required this.color,
-//     required this.values,
-//   });
-
-//   final String title;
-//   final Color color;
-//   final List<double> values;
-// }
-
 class SubMenu {
   SubMenu({required this.title, this.function, required this.img, this.widget});
   final String title;
@@ -45,7 +33,6 @@ class ProfileCreateState extends State<ProfileInfo> {
   GetUser? profile;
   UserRepo userRepo = UserRepo();
   var logger = Logger();
-
 
   getProfile() {
     try {
@@ -62,32 +49,12 @@ class ProfileCreateState extends State<ProfileInfo> {
   @override
   void initState() {
     super.initState();
-    getProfile();
   }
 
   List<Widget> intos() {
     List<Widget> result = [];
     List<SubMenu> subMenus = [
-      SubMenu(
-          title: "個人檔案",
-          img: "assets/images/profile.png",
-          function: () => Navigator.pushNamed(context, Profile.routeName,
-              arguments: profile)),
-      SubMenu(
-          title: "成效分析",
-          img: "",
-          function: () => Navigator.push(
-                context,
-                MaterialPageRoute<void>(
-                  builder: (BuildContext context) => const DailyEarnings(),
-                ),
-              ),
-          widget: const Icon(Icons.accessibility_new_rounded)),
-      SubMenu(
-          title: "管理運動計畫",
-          img: "assets/images/target.png",
-          function: () => Navigator.pushNamed(context, PlanPage.routeName,
-              arguments: profile)),
+      SubMenu(title: "個人檔案", img: "assets/images/profile.png", function: () {}),
       SubMenu(
         title: "登出",
         img: "assets/images/logout.png",
@@ -111,7 +78,6 @@ class ProfileCreateState extends State<ProfileInfo> {
         Box.boxHasRadius(
           margin: const EdgeInsets.fromLTRB(0, 10, 0, 5),
           height: 70,
-          
           padding: const EdgeInsets.all(10),
           child: GestureDetector(
             behavior: HitTestBehavior.translucent,
@@ -149,63 +115,7 @@ class ProfileCreateState extends State<ProfileInfo> {
         child: SingleChildScrollView(
           child: (Column(
             mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-                  (profile == null)
-                      ? Box.boxHasRadius(
-                          // height: MediaQuery.of(context).size.height * 0.33,
-                          color: MyTheme.backgroudColor,
-                          child: Center(
-                              child: CircularProgressIndicator(
-                            color: MyTheme.lightColor,
-                          )),
-                        )
-                      : Box.boxHasRadius(
-                          // height: MediaQuery.of(context).size.height * 0.25,
-                          color: Colors.white,
-                          child: Container(
-                            decoration: BoxDecoration(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(20)),
-                              color: MyTheme.color,
-                            ),
-                            child: Container(
-                              padding: const EdgeInsets.fromLTRB(
-                                  20, 40, 20, 20),
-                              alignment: Alignment.center,
-                              child: Column(
-                                crossAxisAlignment:
-                                    CrossAxisAlignment.start,
-                                mainAxisAlignment:
-                                    MainAxisAlignment.center,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.fromLTRB(
-                                        0, 0, 0, 20),
-                                    child: textWidget(
-                                        text: profile!.name,
-                                        type: TextType.sub,
-                                        color: Colors.white),
-                                  ),
-                                  Padding(
-                                      padding: const EdgeInsets.fromLTRB(
-                                          0, 0, 0, 10),
-                                      child: textWidget(
-                                          text:
-                                              "性別：${profile!.sex != "female" ? "男" : "女"}",
-                                          type: TextType.content,
-                                          color: Colors.white)),
-                                  textWidget(
-                                      text:
-                                          "年齡：${DateTime.now().year - profile!.birthday.year}",
-                                      type: TextType.content,
-                                      color: Colors.white),
-                                ],
-                              ),
-                            ),
-                          ),
-                        ),
-                ] +
-                intos(),
+            children: intos(),
           )),
         ),
       ),
